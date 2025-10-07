b637d0d68fc68ea53be68ff08ee586c6f49741c4
StarCookie256 <sopov.cookie@gmail.com>
2025-10-07 18:52:21 +0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+import { BrowserRouter } from 'react-router-dom';
+
+function App() {
+  return (
+    <Routes>
+      <div className="App">
+          <Router>
+            
+          </Router>
+      </div>
+    </Routes>
+  );
+}
+
+export default App;
---
+++ src/pages/main.jsx
@@ -0,0 +1,9 @@
+import './main.css';
+
+function MainPage(){
+  return(
+    <header>Парфюмерияяяяяя</header>
+  );
+}
+
+export default MainPage;
