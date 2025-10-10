2e608155a45942c1e9626c45e33d263be21c93d7
StarCookie256 <sopov.cookie@gmail.com>
2025-10-10 18:53:27 +0700

    фронтенд - всякое поделал с хедером

--- src/components/perfumeryHeader/perfumeryHeader.jsx
+++ src/components/perfumeryHeader/perfumeryHeader.jsx
@@ -1,9 +1,24 @@
 import './perfumeryHeader.css';
+import { Link } from 'react-router-dom';
 
 export default function PerfumeryHeader(){
+  const headerPages = [
+    {path:'/main', pageName:'mainPage', label:'Главная'}
+  ];
+
+
   return(
     <header className='header-container'>
-      
+      {headerPages.map((page) => (
+        <Link
+          key = {page.path}
+          to = {page.path}
+        >
+          <div className='header-item'>
+            {page.label}
+          </div>
+        </Link>
+      ))}
     </header>
   );
 }
