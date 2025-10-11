df5493c6658bf98f561cbeb4dce7bb3c948caee9
StarCookie256 <sopov.cookie@gmail.com>
2025-10-11 20:01:13 +0700

    чучут меин

--- src/pages/main.jsx
+++ src/pages/main.jsx
@@ -3,7 +3,7 @@ import './main.css';
 function MainPage(){
   return(
     <div className='main-page-container'>
-      <div className='main-page-container'>Каталог товаров</div>
+      <div className='main-page-title'>Самые популярные ароматы на сегодняшний день:</div>
     </div>
   );
 }
