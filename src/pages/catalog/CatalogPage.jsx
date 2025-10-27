843d56c280b86032d1bfb0085f3ab000c87bfda5
StarCookie256 <sopov.cookie@gmail.com>
2025-10-27 23:52:18 +0700

    фронт - немного поработал со страницей КАТАЛОГА

--- src/pages/catalog/CatalogPage.jsx
+++ src/pages/catalog/CatalogPage.jsx
@@ -2,24 +2,40 @@ import './CatalogPage.css';
 import { brandsService } from '../../api/services/brandsService';
 import { useEffect, useState } from 'react';
 
-async function fetchData(setBrands){
+async function fetchData(setBrands, setBrandNavigations){
   let localBrandsList = [];
+  let localBrandNavigationsList = [];
 
-  brandsService.getBrands()
-  .then(brandsData => {
-    setBrands(brandsData);
+  localBrandsList = await brandsService.getBrands();
+
+  localBrandsList = localBrandsList.map(brand => {
+    brand.firstLetter = brand.name.charAt(0)
+
+    if(!localBrandNavigationsList.includes(brand.firstLetter))
+      localBrandNavigationsList.push(brand.firstLetter)
+
+    return brand;
   });
+
+  setBrands(localBrandsList);
+  setBrandNavigations(localBrandNavigationsList);
 }
 
 function CatalogPage(){
   const [brands,setBrands] = useState([]);
+  const [brandNavigations, setBrandNavigations] = useState([]);
 
   useEffect(() => {
-    fetchData(setBrands);
+    fetchData(setBrands, setBrandNavigations);
   }, []);
 
   return(
     <div className='brands-page-container'>
+      <div className='brands-page-navigations'>
+      </div>
+      {brandNavigations.map((brandNavigation) => (
+        <div>{brandNavigation}</div>
+      ))}
       {brands.map((brand) => (
         <a className='brands-page-brand' href={`/brands/${brand.refName}`}>{brand.name}</a>
       ))}
