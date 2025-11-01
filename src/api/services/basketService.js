01124c2b0078bab29b6691827d48276555eaf610
StarCookie256 <sopov.cookie@gmail.com>
2025-11-01 19:45:13 +0700

    фронт - начал делать страницу КОРЗИНЫ, доделать КОРЗИНУ, потом БЭКЭЭЭЭНД

---
+++ src/api/services/basketService.js
@@ -0,0 +1,69 @@
+  // ДЛЯ REAL BASKET SERVICE ПРИГОДИТСЯ
+  // getBasketByUserId: async (userId) => {
+  //   return mockBasketProducts;
+  // },
+
+const mockBasketProducts = {
+  products: [
+    {
+      id: 1,
+      productName: 'Газики фембоев',
+      categoryName: 'Парфюмерная вода',
+      image: 'https://shikimori.one/uploads/poster/characters/79995/main_alt-f083b9fc0baf74cb7d475ef9c368ae7b.jpeg',
+      quantity: 42,
+      price: 42,
+      volume: 10
+    },
+    {
+      id: 2,
+      productName: 'Газики фембоев',
+      categoryName: 'Парфюмерная вода',
+      image: 'https://shikimori.one/uploads/poster/characters/79995/main_alt-f083b9fc0baf74cb7d475ef9c368ae7b.jpeg',
+      quantity: 42,
+      price: 42,
+      volume: 10
+    },
+    {
+      id: 3,
+      productName: 'Газики фембоев',
+      categoryName: 'Парфюмерная вода',
+      image: 'https://shikimori.one/uploads/poster/characters/79995/main_alt-f083b9fc0baf74cb7d475ef9c368ae7b.jpeg',
+      quantity: 42,
+      price: 42,
+      volume: 10
+    }
+  ],
+  totalPrice: 126
+};
+
+const mockBasketService = {
+  getBasket: async () => {
+    return mockBasketProducts;
+  },
+
+  getBasketProductsCount: async () => {
+    const productsCount = mockBasketProducts.products.length;
+
+    return productsCount;
+  },
+
+  updateBasket: async (id, productData) => {
+    await new Promise(resolve => setTimeout(resolve, 600));
+    
+    const productIndex = mockBasketProducts.findIndex(p => p.id === parseInt(id));
+    if (productIndex === -1) {
+      throw new Error('Товар не найден');
+    }
+    
+    const updatedProduct = {
+      ...mockBasketProducts[productIndex],
+      ...productData
+    };
+    
+    console.log('Обновлен товар:', updatedProduct);
+    
+    return updatedProduct;
+  }
+};
+
+export const basketService = mockBasketService;
--- src/components/perfumeryHeader/perfumeryHeader.jsx
+++ src/components/perfumeryHeader/perfumeryHeader.jsx
@@ -1,30 +1,85 @@
 import './perfumeryHeader.css';
 import { Link, useLocation } from 'react-router-dom';
+import { basketService } from '../../api/services/basketService';
+import { useEffect, useState } from 'react';
+
+async function fetchData(setBasketCount){
+  let localBasketCount = await basketService.getBasketProductsCount();
+
+  setBasketCount(localBasketCount);
+}
 
 export default function PerfumeryHeader(){
   const location = useLocation();
-  const headerPages = [
+  const [basketCount, setBasketCount] = useState();
+  const [loading, setLoading] = useState(true);
+  const topHeaderPages = [
     {path:'/main', pageName:'mainPage', label:'Slay💅Star'},
     {path:'/catalog', pageName:'catalog', label:'Каталог товаров'},
     {path:'/cabinet', pageName:'cabinet', label:'Личный кабинет'}
   ];
+  const bottomHeaderPages = [
+    {path:'/search', pageName:'search', label:'Поиск'},
+    {path:'/basket', pageName:'basket', label:'Корзина'}
+  ];
 
+  useEffect(() => {
+    try{
+      setLoading(true);
+      fetchData(setBasketCount);
+    }
+    catch(error){
+      console.error('Error loading user data:', error);
+    }
+    finally{
+      setLoading(false);
+    }
+  }, [basketCount]);
+
+  if(loading){
+    return(
+      <div className='cabinet-page-container'>
+        Загрузка...
+      </div>
+    );
+  }
 
   return(
     <header className='header-container'>
-      <div className='header-items-container'>
-        {headerPages.map((page) => (
-          <Link
-            className={`header-item-link ${location.pathname === page.path ? 'active' : ''}`}
-            key = {page.path}
-            to = {page.path}
-          >
-            <div className='header-item'>
-              {page.label}
-            </div>
-          </Link>
-        ))}
+
+      <div className='header-top-container'>
+        <div className='header-top-items'>
+          {topHeaderPages.map((page) => (
+            <Link
+              className={`header-item-link ${location.pathname === page.path ? 'active' : ''}`}
+              key = {page.path}
+              to = {page.path}
+            >
+              <div className={`header-item ${page.pageName}`}>
+                {page.label}
+              </div>
+            </Link>
+          ))}
+        </div>
       </div>
+
+      <div className='header-bottom-container'>
+          <div className='header-bottom-items'>
+            {bottomHeaderPages.map((page) => (
+            <Link
+              className={`header-item-link ${location.pathname === page.path ? 'active' : ''}`}
+              key = {page.path}
+              to = {page.path}
+            >
+              <div className={`header-item ${page.pageName}`}>
+                {page.label}
+                {page.pageName === 'basket' && <div className='header-basket-count'>{basketCount}</div>}
+              </div>
+            </Link>
+          ))}
+          </div>
+      </div>
+      
     </header>
   );
 }
